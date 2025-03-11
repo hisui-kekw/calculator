9f9ae6c14d9d98af2c6eca5f78a1783e7a1ac74f
hisui-kekw <https://github.com/hisui-kekw/test.git>
2025-03-11 22:05:00 +0800

    test

---
+++ calculator/calculator.js
@@ -0,0 +1,18 @@
+// Function to append values to the display
+function appendToDisplay(value) {
+    display.value += value;
+  }
+  
+  // Function to clear the display
+  function clearDisplay() {
+    display.value = "";
+  }
+  
+  // Function to evaluate the expression
+  function calculate() {
+    try {
+      display.value = eval(display.value);
+    } catch (error) {
+      alert("Invalid expression");
+    }
+  }
